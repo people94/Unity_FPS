6e646c129115fb68739e2c7e76d41c810a2fc8f4
Seung Yoon Baek <bsyu0804@naver.com>
2020-06-16 16:57:21 +0900

    플레이어 총알 폭탄 생성
    - 플레이어 레이캐스트, 레이캐스트 히트를 이용한 총알 발사
    - rigidbody, addForce를 이용한 포물선 폭탄던지기
    - 총알은 끝지점, 폭탄은 충돌지점에 이펙트 발생

---
+++ FPS/Assets/_BSY/Scripts/BulletMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(transform.forward * 5 * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //gameObject.SetActive(false);
+    }
+}
